7bc5ecae85dd77f718a1090f7d953e04b936c61b
david-andre <53012718+david-andre@users.noreply.github.com>
2020-06-02 16:55:05 -0500

    Creación de las carpetas Queries y Trasnsactions que contienen QBLL y TBLL del proyecto y generamos las vistas por medio del scaffold en la carpeta Contollers

--- BEUEjercicio/Alumno.cs
+++ BEUEjercicio/Alumno.cs
@@ -11,7 +11,8 @@ namespace BEUEjercicio
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Alumno
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,12 +21,32 @@ namespace BEUEjercicio
             this.Matriculas = new HashSet<Matricula>();
         }
     
+        [ScaffoldColumn(false)]
         public int idalumno { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Los nombres es requeridos"), MaxLength(55)]
+        [Display(Name = "Nombres")]
         public string nombre { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Los apellidos es requeridos"), MaxLength(55)]
+        [Display(Name = "Apellidos")]
         public string apellidos { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Los cédula es requerida"), MaxLength(15)]
+        [Display(Name = "Cédula")]
         public string cedula { get; set; }
+
+        [Display(Name = "Fecha de Nacimiento")]
         public Nullable<System.DateTime> fecha_nacimiento { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Lugar de nacimiento")]
         public string lugar_nacimiento { get; set; }
+
+        [Display(Name = "Sexo")]
         public string sexo { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
---
+++ BEUEjercicio/Transactions/AlumnoTBLL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUEjercicio.Transactions
+{
+    class AlumnoTBLL
+    {
+        //TBLL TRANSACTIONAL BUSSINESS LOGIC LAYER (TRANSACCIONES - ESCRITURA)
+        private Entities context;
+        public Alumno alumno { get; set; } //Alumno activo
+
+        public AlumnoTBLL( bool withContext)
+        {
+            if(withContext)
+            {
+                context = new Entities();
+            }
+        }
+
+        public void Get(int id)
+        {
+            alumno = context.Alumnoes.Find(id);
+        }
+
+        public void Create(Alumno a)
+        {
+            using(Entities db = new Entities())
+            {
+                using(var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Alumnoes.Add(a);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public void Update()
+        {
+            using(var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    context.Entry(alumno).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+                    transaction.Commit();
+                }
+                catch(Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+        }
+
+        public void Delete()
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    context.Entry(alumno).State = System.Data.Entity.EntityState.Deleted;
+                    context.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+        }
+    }
+}
